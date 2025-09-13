b67c57dafbd5836749b9bf1789b8e8ed32353f55
brihat2590 <thapabrihatbir321@gmail.com>
2025-09-13 22:59:03 +0530

    minor update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import {auth} from "@/lib/auth"
 import LandingPage from "@/components/Landing"
 import Features from "@/components/Features"
-import PricingSection from "@/components/PricingTable"
+
 import Footer from "@/components/Footer"
   
 export default function page(){
