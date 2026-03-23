8ba82d43abf7257d34781f9c3f95bb0b2270077f
brihat2590 <thapabrihatbir321@gmail.com>
2026-03-23 22:20:51 +0530

    chore:minor fix

--- src/app/api/messages/[sessionId]/route.ts
+++ src/app/api/messages/[sessionId]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma"
 
-export async function GET({ params }: { params: Promise<{ sessionId: string }> }) {
+export async function GET(request:NextRequest,{ params }: { params: Promise<{ sessionId: string }> }) {
     const { sessionId } = await params;
     const messages = await prisma.message.findMany({
         where: {
